a43353fbe22c3a04ac38476bbe813a5843a34afd
MatthewKangas <kangasmatthew@gmail.com>
2019-11-15 10:13:23 -0800

    =Added character frequency analysis functions, currently finds closest match to standard english text

--- Set1/Set1.py
+++ Set1/Set1.py
@@ -1,23 +1,16 @@
 import base64
 
-
-
-
-#Set 1 Challenge 1
+###Set 1 Challenge 1###
 
 test = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-
 testbin = base64.b16decode(test, True)
-
 test64 = base64.b64encode(testbin)
 
 #print(test)
-
 #print(testbin)
-
 #print(test64)
 
-#Set 1 Challenge 2
+###Set 1 Challenge 2###
 
 def fixedXor(bin1, bin2):
     bin1Array = bytearray(bin1)
@@ -25,7 +18,7 @@ def fixedXor(bin1, bin2):
     resultBinArray = bytearray()
     for bit in bin1Array:
         resultBinArray.append(bit ^ bin2Array[0])
-        bin2array.pop(0)
+        bin2Array.pop(0)
     return resultBinArray
 
 
@@ -35,5 +28,80 @@ testCh22 = "686974207468652062756c6c277320657965"
 testChXor = fixedXor(base64.b16decode(testCh21, True),
                      base64.b16decode(testCh22, True))
 
-print(base64.b16encode(testChXor))
+#print(base64.b16encode(testChXor))
 
+###Set 1 Challenge 3###
+
+##Load in the english character frequency table##
+FrequencyTable = {}
+rawFreqFile = open("FrequencyTable.txt","r")
+FreqFileList = rawFreqFile.readlines()
+for line in FreqFileList:
+    freqComponent = line.split(",")
+    letter = freqComponent[0]
+    frequency = float(freqComponent[1].rstrip())
+    FrequencyTable[letter] = frequency
+
+
+
+keySpace = []
+start = int('41', 16)
+end = int('7a', 16)
+for index in range(start, end + 1):
+    keySpace.append(index)
+
+cryptTextHex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+cryptTextBin = base64.b16decode(cryptTextHex, True)
+
+def singleCharacterXorDecrypt(cryptText, binCharacter):
+    cryptArray = bytearray(cryptTextBin)
+    plainArray = bytearray()
+    for byte in cryptArray:
+        plainArray.append(byte ^ binCharacter)
+    return plainArray
+
+def getTextCharacterFrequency(text):
+    textDict = {}
+    length = len(text)
+    for char in text:
+        keys = textDict.keys()
+        if char in keys:
+            textDict[char] += 1
+        else:
+            textDict[char] = 1
+
+    for key in textDict:
+        textDict[key] = textDict[key]/length
+
+    return textDict
+
+
+
+def compareCharFreq(textDict, referenceDict):
+    distance = 0
+    for key in referenceDict:
+        variance = 0
+        if key in textDict:
+            variance = abs(textDict[key] - referenceDict[key])
+        else:
+            variance = abs(referenceDict[key])
+        distance += variance
+
+    return distance
+
+closestMatch = "NULL"
+closestDistance = 999
+for key in keySpace:
+    plainBin = singleCharacterXorDecrypt(cryptTextHex, bytes(chr(key), 'utf-8')[0])
+    plainStr = str(plainBin, 'utf-8').lower().strip()
+    distance = compareCharFreq(getTextCharacterFrequency(plainStr),
+                               FrequencyTable)
+    if distance < closestDistance:
+        print("New closest match: " + chr(key) + " " + str(distance))
+        closestDistance = distance
+        closestMatch = chr(key)
+
+plainBin = singleCharacterXorDecrypt(cryptTextHex, bytes(closestMatch, 'utf-8')[0])
+plainStr = str(plainBin, 'utf-8').lower().strip()
+print(plainStr)
